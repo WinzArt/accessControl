b5c43144c0708f0f46bfaf7d95e3ebabadbbc699
Muhammad Al Ridha <muhammad.alridha@gmail.com>
2022-06-02 17:55:17 +0700

    update hapus navbar dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
 		@include("layouts.header")
 		<!--end header -->
 		<!--navigation-->
-		@include("layouts.nav")
+		<!-- @include("layouts.nav") -->
 		<!--end navigation-->
 		<!--start page wrapper -->
 		@yield("wrapper")
